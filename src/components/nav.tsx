7027b83573c4f9d290e6730f57001123623f64b1
Mitesh Trivedi <miteshtrivedi10@users.noreply.github.com>
2025-06-22 17:18:28 +0530

    Modified source

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -26,9 +26,6 @@ export default function Nav() {
     <header className="fixed inset-x-0 top-0 z-50 backdrop-blur">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <Link href="/" className="-m-1.5 p-1.5 text-lg font-semibold">
-            Mitesh Trivedi
-          </Link>
         </div>
         <div className="flex gap-8">
           {navigation.map((item) => (
